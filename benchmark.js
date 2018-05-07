625fd84917a70e54271bf4e00d30a01882b6f16f
jlyonas <jlyon@acumensolutions.com>
2018-05-07 09:58:50 -0400

    add transform, filter, and sort comparisons

--- benchmark.js
+++ benchmark.js
@@ -8,7 +8,13 @@ for (var i = 0; i < arr.length; ++i) {
   arr[i] = (Math.random() * 10001) | 0
 }
 
-suite.add('While loop imitating a for loop', () => {
+const compare = function(a, b) {
+  return a > b;
+};
+
+suite
+// *** Loops ***
+.add('While loop imitating a for loop', () => {
   let i = 0
 
   while (i < arr.length) {
@@ -76,4 +82,51 @@ suite.add('While loop imitating a for loop', () => {
 
   arr.forEach(foo)
 })
+
+// *** Transform ***
+.add('Transform by For Loop', () => {
+  const arrOfObjects = [];
+  for(let i = 0; i < arr.length; i++) {
+    arrOfObjects.push({ value: arr[i] });
+  }
+
+}).add('Transform by array.map', () => {
+  const arrOfObjects = arr.map(a => ({ value: a }));
+
+}).add('Transform by array.reduce', () => {
+  const flattenedArray = arr.reduce((acc, index) => acc += index, 0);
+})
+
+// *** Filter ***
+.add('Filter by For Loop', () => {
+  const filteredArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] < 5000) {
+      filteredArray.push({ value: arr[i] });
+    }
+  }
+}).add('Filter by array.filter', () => {
+  const filteredArray = arr.filter(a => a < 5000);
+})
+
+// *** Sort ***
+.add('Sort by For Loop Bubble Sort', () => {
+  const sortedArray = arr;
+  let swapped;
+  do {
+    swapped = false;
+    for(let i = 0; i < sortedArray.length - 1; i++) {
+      const nextIndex = i + 1;
+      if(compare(sortedArray[i], sortedArray[nextIndex])) {
+        const temp = sortedArray[i];
+        sortedArray[i] = sortedArray[nextIndex];
+        sortedArray[nextIndex] = temp;
+        swapped = true;
+      }
+    }
+  } while(swapped);
+}).add('Sort by array.sort', () => {
+  const sortedArray = arr.sort(compare);
+})
+
 .on('cycle', event => console.log(String(event.target))).run({ async: true })
